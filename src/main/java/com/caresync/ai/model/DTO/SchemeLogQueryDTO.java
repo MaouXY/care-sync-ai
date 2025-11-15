ff07f4f9ea93394568bbc3286d1a9fc1dd13c440
MaouXY <nax666@qq.com>
2025-11-16 04:40:36 +0800

    feat(ai): 添加SchemeLogQueryDTO查询参数类

---
+++ src/main/java/com/caresync/ai/model/DTO/SchemeLogQueryDTO.java
@@ -0,0 +1,30 @@
+package com.caresync.ai.model.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 服务方案列表查询请求参数
+ */
+@Data
+@Schema(description = "服务方案日志列表查询请求参数")
+public class SchemeLogQueryDTO {
+    @Schema(description = "儿童ID")
+    private Long childId; // 儿童ID
+    @Schema(description = "服务人员ID")
+    private Long workerId; // 社工ID
+    @Schema(description = "服务目标")
+    private String target;
+    @Schema(description = "方案名称/儿童名称（模糊查询）")
+    private String name; // 方案名称/儿童名称（模糊查询）
+    @Schema(description = "创建开始日期")
+    private LocalDateTime startDate; // 创建开始日期
+    @Schema(description = "创建结束日期")
+    private LocalDateTime endDate; // 创建结束日期
+    @Schema(description = "页码")
+    private Integer page=1; // 页码
+    @Schema(description = "每页条数")
+    private Integer pageSize=10; // 每页条数
+}
